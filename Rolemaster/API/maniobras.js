827b768581253e7a3940f85faa22e4adae5b47ca
Alejandro Gamino <alejandrogamino@pm.me>
2023-09-12 01:00:22 +0200

    Maniobras de movimiento y correr

--- Rolemaster/API/maniobras.js
+++ Rolemaster/API/maniobras.js
@@ -1,133 +1,446 @@
+const { evaluate } = require("mathjs");
+var fs = require("fs");
+
 let tablaManiobrasMovimiento = {
-	indices: [-151,-101,-51,-26,0,20,40,55,,65,75,85,95,105,115,125,135,145,155,165,185,225,275,9999999],
-	rut: ["F","10","30","50","70","80","90","100","100","100","100","100","110","110","120","120","130","130","140","140","150","150","160"], 
-	fac: ["F","F","10","30","50","60","70","80","90","100","100","100","100","110","110","120","120","130","130","140","140","150","150"], 
-	edf: ["F","F","F","10","30","50","60","70","80","90","100","100","100","100","110","110","120","120","130","130","140","140","150"], 
-	dfm: ["F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100","110","110","120","120","130","130","140"], 
-	dif: ["F","F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100","110","110","120","120","130","130"], 
-	mdf: ["F","F","F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100","110","110","120","120","130"], 
-	xdf: ["F","F","F","F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100","110","110","120","120"], 
-	loc: ["F","F","F","F","F","F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100","110","110"], 
-	abs: ["F","F","F","F","F","F","F","F","F","F","F","5","10","20","30","40","50","60","70","80","90","100","100"], 
-	getResultado(roll,dificultad) {
-		let tabla = null;
-		if (dificultad=="rut") tabla = this.rut;
-		else if (dificultad=="fac") tabla = this.fac;
-		else if (dificultad=="edf") tabla = this.edf;
-		else if (dificultad=="dfm") tabla = this.dfm;
-		else if (dificultad=="dif") tabla = this.dif;
-		else if (dificultad=="mdf") tabla = this.mdf;
-		else if (dificultad=="xdf") tabla = this.xdf;
-		else if (dificultad=="loc") tabla = this.loc;
-		else if (dificultad=="abs") tabla = this.abs;
-		return tabla[this.indices.findIndex(function(value) {
-			return value >= roll;
-		})];
-	}		
+  indices: [
+    -151,
+    -101,
+    -51,
+    -26,
+    0,
+    20,
+    40,
+    55,
+    ,
+    65,
+    75,
+    85,
+    95,
+    105,
+    115,
+    125,
+    135,
+    145,
+    155,
+    165,
+    185,
+    225,
+    275,
+    9999999,
+  ],
+  rut: [
+    "F",
+    "10",
+    "30",
+    "50",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "100",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+    "130",
+    "140",
+    "140",
+    "150",
+    "150",
+    "160",
+  ],
+  fac: [
+    "F",
+    "F",
+    "10",
+    "30",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+    "130",
+    "140",
+    "140",
+    "150",
+    "150",
+  ],
+  edf: [
+    "F",
+    "F",
+    "F",
+    "10",
+    "30",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+    "130",
+    "140",
+    "140",
+    "150",
+  ],
+  dfm: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+    "130",
+    "140",
+  ],
+  dif: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+    "130",
+  ],
+  mdf: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+    "130",
+  ],
+  xdf: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "110",
+    "110",
+    "120",
+    "120",
+  ],
+  loc: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+    "110",
+    "110",
+  ],
+  abs: [
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "F",
+    "5",
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "100",
+  ],
+  getResultado(roll, dificultad) {
+    let tabla = null;
+    if (dificultad == "rut") tabla = this.rut;
+    else if (dificultad == "fac") tabla = this.fac;
+    else if (dificultad == "edf") tabla = this.edf;
+    else if (dificultad == "dfm") tabla = this.dfm;
+    else if (dificultad == "dif") tabla = this.dif;
+    else if (dificultad == "mdf") tabla = this.mdf;
+    else if (dificultad == "xdf") tabla = this.xdf;
+    else if (dificultad == "loc") tabla = this.loc;
+    else if (dificultad == "abs") tabla = this.abs;
+    return tabla[
+      this.indices.findIndex(function (value) {
+        return value >= roll;
+      })
+    ];
+  },
 };
 
-on("chat:message", function(msg) {
-  if(msg.type == "api" && msg.content.indexOf("!mm ") !== -1) {
-	  // !mm dif000100
-	    if(_.has(msg,'inlinerolls')){
-            msg.content = _.chain(msg.inlinerolls)
-                .reduce(function(m,v,k){
-                    m['$[['+k+']]']=v.results.total || 0;
-                    return m;
-                },{})
-                .reduce(function(m,v,k){
-                    return m.replace(k,v);
-                },msg.content)
-            .value();
-        }
-    msg.content = msg.content.replace("!mm ", "");
-	let dificultad = msg.content.substring(0,3);
-	let strDificultad = "";
-	if (dificultad=="rut") strDificultad = "Rutinaria";
-	else if (dificultad=="fac") strDificultad = "Fácil";
-	else if (dificultad=="edf") strDificultad = "Escasa dificultad";
-	else if (dificultad=="dfm") strDificultad = "Dificultad media";
-	else if (dificultad=="dif") strDificultad = "Difícil";
-	else if (dificultad=="mdf") strDificultad = "Muy difícil";
-	else if (dificultad=="xdf") strDificultad = "Extremadamente difícil";
-	else if (dificultad=="loc") strDificultad = "Locura completa";
-	else if (dificultad=="abs") strDificultad = "Absurdo";
-	else {
-			log("ERROR !mm(main): dificultad = " + dificultad);
-			return;
-		}
-	let aturdido = msg.content.substring(3,4);
-	let derribado = msg.content.substring(4,5);
-	let extremidadInutilizada = msg.content.substring(5,6);
-	let tirada = msg.content.substring(6);
-	let indice = parseInt(tirada) + (parseInt(aturdido) * (-50)) + (parseInt(derribado) * (-70)) + (parseInt(extremidadInutilizada) * (-30));
-	let chorrazo = "";
-	chorrazo += "/w GM &{template:maniobramovimiento}";
-	chorrazo += "{{dificultad=" + strDificultad + "}}";
-	chorrazo += "{{modificadores=";
-	if (aturdido > 0) chorrazo +="\uD83E\uDD74 ";
-	if (derribado > 0) chorrazo +="\uD83D\uDECC\uD83C\uDFFB ";
-	if (extremidadInutilizada>0) chorrazo += "\uD83D\uDC68\u200D\uD83E\uDDBD";
-	chorrazo += "}}";
-	chorrazo += "{{tirada=" + tirada + "}}";
-	chorrazo += "{{tirada modificada= " + indice + "}}";
-	chorrazo += "{{resultado= " + tablaManiobrasMovimiento.getResultado(indice,dificultad) + "}}";
-	log(chorrazo);
-	// aturdido: \uD83E\uDD74
-	// derribado: 	\uD83D\uDECC\uD83C\uDFFB
-	// miembro inutilizado: \uD83D\uDC68\uD83C\uDFFB\u200D\uD83E\uDDBD
-	
-	
-	sendChat(msg.who, chorrazo);
-		
-	} else if(msg.type == "api" && msg.content.indexOf("!correr ") !== -1) {
-	    // !correr (rut|fac...)000(mm 3 dígitos)30
-	    if(_.has(msg,'inlinerolls')){
-            msg.content = _.chain(msg.inlinerolls)
-                .reduce(function(m,v,k){
-                    m['$[['+k+']]']=v.results.total || 0;
-                    return m;
-                },{})
-                .reduce(function(m,v,k){
-                    return m.replace(k,v);
-                },msg.content)
-            .value();
-        }
-		msg.content = msg.content.replace("!correr ", "");
-		
-		let dificultad = msg.content.substring(0,3);
-		let strDificultad = "";
-		if (dificultad=="rut") strDificultad = "Rutinaria";
-		else if (dificultad=="fac") strDificultad = "Fácil";
-		else if (dificultad=="edf") strDificultad = "Escasa dificultad";
-		else if (dificultad=="dfm") strDificultad = "Dificultad media";
-		else if (dificultad=="dif") strDificultad = "Difícil";
-		else if (dificultad=="mdf") strDificultad = "Muy difícil";
-		else if (dificultad=="xdf") strDificultad = "Extremadamente difícil";
-		else if (dificultad=="loc") strDificultad = "Locura completa";
-		else if (dificultad=="abs") strDificultad = "Absurdo";
-		else {
-			log("ERROR !correr(main): dificultad = " + dificultad);
-			return;
-		}
-		let aturdido = msg.content.substring(3,4);
-		let derribado = msg.content.substring(4,5);
-		let extremidadInutilizada = msg.content.substring(5,6);
-		let mm = parseInt(msg.content.substring(6,9));
-		let tirada = msg.content.substring(9);
-		let indice = parseInt(tirada) + (parseInt(aturdido) * (-50)) + (parseInt(derribado) * (-70)) + (parseInt(extremidadInutilizada) * (-30));
-		let result = tablaManiobrasMovimiento.getResultado(indice,dificultad);
-		let resultado = "";
-		if (result == "F") resultado = "Pifia";
-		else {
-			let resultInt = parseInt(result);
-			let diferencia100 = resultInt - 100;
-			let tramos = diferencia100/10;
-			let metros = 2*(50 + mm)*0.3 + (tramos*3);
-			resultado = metros.toString() + " metros";
-		}	
-		sendChat(msg.who, "/w GM &{template:default}{{name=Correr}}{{Correr="+resultado+"}}");
+function maniobras(msg) {
+  if (msg.indexOf("!mm ") !== -1) {
+    msg = msg.replace("!mm ", "");
+    // !mm dif+30000100/+30-10
+    // !mm rut-1011185/+45-10+38-12
+    // !mm (dificultad[3])(bonif_mm[3])(aturdido[1])(derribado[1])(extremidadInutilizada[1])(Tirada[variable])/(Modificadores[variable])
+    let dificultad = msg.substring(0, 3);
+    let strDificultad = "";
+    if (dificultad == "rut") strDificultad = "Rutinaria";
+    else if (dificultad == "fac") strDificultad = "Fácil";
+    else if (dificultad == "edf") strDificultad = "Escasa dificultad";
+    else if (dificultad == "dfm") strDificultad = "Dificultad media";
+    else if (dificultad == "dif") strDificultad = "Difícil";
+    else if (dificultad == "mdf") strDificultad = "Muy difícil";
+    else if (dificultad == "xdf") strDificultad = "Extremadamente difícil";
+    else if (dificultad == "loc") strDificultad = "Locura completa";
+    else if (dificultad == "abs") strDificultad = "Absurda";
+    else {
+      console.log("ERROR !mm(main): dificultad = " + dificultad);
+      return;
+    }
+    let bonifMM = msg.substring(3, 6);
+    let aturdido = msg.substring(6, 7);
+    let derribado = msg.substring(7, 8);
+    let extremidadInutilizada = msg.substring(8, 9);
+    let tirada = msg.substring(9);
+    let modStr = "0";
+    let posSlash = msg.indexOf("/");
+    if (posSlash >= 0) {
+      tirada = msg.substring(9, posSlash);
+      modStr = msg.substring(posSlash + 1);
+    }
 
-	}
-	
+    let modAturdido = parseInt(aturdido) * -50;
+    let modDerribado = parseInt(derribado) * -70;
+    let modExtremInut = parseInt(extremidadInutilizada) * -30;
+    let modificadores =
+      evaluate(modStr) +
+      modAturdido +
+      modDerribado +
+      modExtremInut +
+      parseInt(bonifMM);
+
+    let indice = parseInt(tirada) + modificadores;
+    let chorrazo = "";
+    chorrazo += "\n===== Maniobra de movimiento " + strDificultad + " =====\n";
+    chorrazo += " - " + getTimestamp() + "\n";
+    if (aturdido == 1) {
+      chorrazo += " - Aturdido\n";
+    }
+    if (derribado == 1) {
+      chorrazo += " - Derribado\n";
+    }
+    if (extremidadInutilizada == 1) {
+      chorrazo += " - Extremidad inutilizada\n";
+    }
+    chorrazo +=
+      " - Tirada: " +
+      tirada +
+      (modificadores >= 0 ? "+" : "") +
+      modificadores +
+      " = " +
+      indice +
+      "\n";
+    chorrazo +=
+      " - Resultado: " +
+      tablaManiobrasMovimiento.getResultado(indice, dificultad) +
+      "\n\n";
+    return chorrazo;
+  } else if (msg.indexOf("!correr ") !== -1) {
+    // !correr (rut|fac...)000(mm 3 dígitos)30
+    msg = msg.replace("!correr ", "");
+
+    let dificultad = msg.substring(0, 3);
+    let strDificultad = "";
+    if (dificultad == "rut") strDificultad = "Rutinaria";
+    else if (dificultad == "fac") strDificultad = "Fácil";
+    else if (dificultad == "edf") strDificultad = "Escasa dificultad";
+    else if (dificultad == "dfm") strDificultad = "Dificultad media";
+    else if (dificultad == "dif") strDificultad = "Difícil";
+    else if (dificultad == "mdf") strDificultad = "Muy difícil";
+    else if (dificultad == "xdf") strDificultad = "Extremadamente difícil";
+    else if (dificultad == "loc") strDificultad = "Locura completa";
+    else if (dificultad == "abs") strDificultad = "Absurda";
+    else {
+      console.log("ERROR !mm(main): dificultad = " + dificultad);
+      return;
+    }
+    let bonifMM = msg.substring(3, 6);
+    let aturdido = msg.substring(6, 7);
+    let derribado = msg.substring(7, 8);
+    let extremidadInutilizada = msg.substring(8, 9);
+    let tirada = msg.substring(9);
+    let modStr = "0";
+    let posSlash = msg.indexOf("/");
+    if (posSlash >= 0) {
+      tirada = msg.substring(9, posSlash);
+      modStr = msg.substring(posSlash + 1);
+    }
+
+    let modAturdido = parseInt(aturdido) * -50;
+    let modDerribado = parseInt(derribado) * -70;
+    let modExtremInut = parseInt(extremidadInutilizada) * -30;
+    let modificadores =
+      evaluate(modStr) +
+      modAturdido +
+      modDerribado +
+      modExtremInut +
+      parseInt(bonifMM);
+
+    let indice = parseInt(tirada) + modificadores;
+
+    let result = tablaManiobrasMovimiento.getResultado(indice, dificultad);
+    let resultado = "";
+    if (result == "F") resultado = "F";
+    else {
+      let resultInt = parseInt(result);
+      let diferencia100 = resultInt - 100;
+      let tramos = diferencia100 / 10;
+      let pies = 2 * (50 + parseInt(bonifMM)) + tramos * 10;
+      let metros = Math.round(0.3048 * pies);
+      resultado = pies.toString() + " pies (" + metros.toString() + " metros)";
+    }
+
+    let chorrazo = "\n\n===== Correr (" + strDificultad + ") =====\n";
+    chorrazo += " - " + getTimestamp() + "\n";
+    if (aturdido == 1) {
+      chorrazo += " - Aturdido\n";
+    }
+    if (derribado == 1) {
+      chorrazo += " - Derribado\n";
+    }
+    if (extremidadInutilizada == 1) {
+      chorrazo += " - Extremidad inutilizada\n";
+    }
+    chorrazo +=
+      " - Tirada: " +
+      tirada +
+      (modificadores >= 0 ? "+" : "") +
+      modificadores +
+      " = " +
+      indice +
+      "\n";
+    chorrazo += " - Resultado: " + resultado + "\n\n";
+    return chorrazo;
+  }
+}
+
+function getTimestamp() {
+  let currentdate = new Date();
+  return (
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1).toString().padStart(2, "0") +
+    "/" +
+    currentdate.getFullYear().toString().padStart(4, "0") +
+    " - " +
+    currentdate.getHours().toString().padStart(2, "0") +
+    ":" +
+    currentdate.getMinutes().toString().padStart(2, "0") +
+    ":" +
+    currentdate.getSeconds().toString().padStart(2, "0") +
+    "." +
+    currentdate.getMilliseconds().toString().padStart(3, "0")
+  );
+}
+
+let text = maniobras(process.argv[2]);
+console.log(text);
+fs.appendFile("man_mov.log", text, "latin1", function (err) {
+  if (err) throw err;
 });
---
+++ Rolemaster/API/mmov.bat
@@ -0,0 +1,159 @@
+@echo off
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 1252>nul
+cls
+echo ===== MANIOBRAS MOVIMIENTO =====
+
+:: MENSAJE
+echo.
+echo Introduzca un mensaje descriptivo de la acción.
+set /P "Mensaje=Mensaje: "
+
+if defined Mensaje (
+  echo. >> MERP.log
+  echo. >> MERP.log
+  echo %Mensaje% >> MERP.log
+)
+
+:: DIFICULTAD
+echo.
+set "Dificultad="
+echo Seleccione la dificultad de la maniobra
+echo   1:Rutina  2:Fácil  3:Escasa Dificultad  4:Dificultad media  5:Difícil
+echo   6:Muy difícil  7:Extremadamente difícil  8:Locura completa  9:Absurda
+set /P "Dificultad=Seleccione (1-9): "
+
+if %Dificultad%==1 (
+    set "Dif=rut"
+    goto :bonif
+)
+if %Dificultad%==2 (
+    set "Dif=fac"
+    goto :bonif
+)
+if %Dificultad%==3 (
+    set "Dif=edf"
+    goto :bonif
+)
+if %Dificultad%==4 (
+    set "Dif=dfm"
+    goto :bonif
+)
+if %Dificultad%==5 (
+    set "Dif=dif"
+    goto :bonif
+)
+if %Dificultad%==6 (
+    set "Dif=mdf"
+    goto :bonif
+)
+if %Dificultad%==7 (
+    set "Dif=xdf"
+    goto :bonif
+)
+if %Dificultad%==8 (
+    set "Dif=loc"
+    goto :bonif
+)
+if %Dificultad%==9 (
+    set "Dif=abs"
+    goto :bonif
+)
+
+echo Dificultad incorrecta: %Dificultad%
+echo.
+exit
+
+
+:: BONIF MM
+:bonif
+echo.
+set "BonifMM="
+echo Introduzca el valor del Bonificador de MM
+echo     (signo + 2 dígitos, ejemplos: +20, -10, -05, +00)
+set /P "BonifMM=Bonificador MM: "
+
+SET "var="&for /f "delims=-+0123456789" %%i in ("%BonifMM%") do set var=%%i
+if defined var (
+   echo Bonificador MM incorrecto: %BonifMM%
+   echo Debe ser signo y dos dígitos
+   echo Ejemplos:
+   echo     +20
+   echo     -10
+   echo     -05
+   echo     +00
+   echo.
+   exit
+)
+:: echo La tirada es %Tirada%
+
+:: ATURDIDO
+echo.
+set "Aturdido="
+set /P "Aturdido=¿Aturdido? (0:No  1:Sí): "
+
+if not %Aturdido%==0 (
+if not %Aturdido%==1 (
+  echo Introduzca 0 o 1
+  echo.
+  exit
+))
+
+:: DERRIBADO
+echo.
+set "Derribado="
+set /P "Derribado=¿Derribado? (0:No  1:Sí): "
+
+if not %Derribado%==0 (
+if not %Derribado%==1 (
+  echo Introduzca 0 o 1
+  echo.
+  exit
+))
+
+:: EXTREMIDAD INUTILIZADA
+echo.
+set "ExtremidadInutilizada="
+set /P "ExtremidadInutilizada=¿Extremidad inutilizada? (0:No  1:Sí): "
+
+if not %ExtremidadInutilizada%==0 (
+if not %ExtremidadInutilizada%==1 (
+  echo Introduzca 0 o 1
+  echo.
+  exit
+))
+
+
+:: TIRADA
+echo.
+set "Tirada="
+set /P "Tirada=Tirada: "
+
+SET "var="&for /f "delims=0123456789" %%i in ("%Tirada%") do set var=%%i
+if defined var (
+   echo Valor de tirada incorrecto: %Tirada%
+   echo Sólo se permiten valores numéricos para la tirada.
+   echo.
+   exit
+)
+
+
+:: MODIFICADORES
+echo.
+set "Modificadores="
+set /P "Modificadores=Modificadores: "
+
+SET "var="&for /f "delims=+-0123456789" %%i in ("%Modificadores%") do set var=%%i
+if defined var (
+   echo Modificadores incorrectos: %Modificadores%
+   echo Sólo se permiten valores numéricos y los caracteres "+" y "-" para los modificadores.
+   echo.
+   exit
+)
+:: RESULTADOS
+echo.
+node maniobras "!mm %Dif%%BonifMM%%Aturdido%%Derribado%%ExtremidadInutilizada%%Tirada%/%Modificadores%"
+node maniobras "!mm %Dif%%BonifMM%%Aturdido%%Derribado%%ExtremidadInutilizada%%Tirada%/%Modificadores%" >> MERP.log
+
+
+chcp %cp%>nul
